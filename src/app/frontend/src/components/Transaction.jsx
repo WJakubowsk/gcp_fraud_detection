d8db11d4e2f402576259315da275f23076e7fa0b
przybytniowskaj <01161611@pw.edu.pl>
2024-05-05 17:01:01 +0200

    add setup for main page

--- src/app/frontend/src/components/Transaction.jsx
+++ src/app/frontend/src/components/Transaction.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "../styles/Transaction.css";
+import "../styles/TransactionHistory.css";
+
 
 function Transaction({transaction, onDelete}) {
-    const formattedDate = new Date(transaction.date).toLocaleDateString("en-US");
+    const formattedDate = new Date(transaction.date).toLocaleDateString();
+    const formattedAmount = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(transaction.amount);
 
     return (
         <div className="transaction-container">
-            <p className="transaction-amount">{transaction.amount}</p>
-            <p className="transaction-description">{transaction.description}</p>
-            <p className="transaction-date">{formattedDate}</p>
-            <button className="delete-button" onClick={() => onDelete(transaction.id)}>
-                Delete
-            </button>
+            <div className="transaction-left">
+                <p className="transaction-date">{formattedDate}</p>
+                <p className="transaction-description">{transaction.description}</p>
+            </div>
+            <div className="transaction-right">
+                <p className="transaction-amount">{formattedAmount}</p>
+            </div>
         </div>
     )
 }
--- src/app/frontend/src/pages/Home.jsx
+++ src/app/frontend/src/pages/Home.jsx
@@ -1,32 +1,35 @@
 import {useState, useEffect, useRef} from "react";
+import {useNavigate} from "react-router-dom";
 import api from "../api";
 import Transaction from "../components/Transaction";
+import BTCTracker from "../components/BTCTracker";
 import "../styles/Home.css";
+import "../styles/Header.css";
 import Chart from "chart.js/auto";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import avatar from '../assets/avatar.png';
+import { Button } from "@mui/material";
+import btcLogo from '../assets/Bitcoin.png';
 
 function Home() {
     const [transactions, setTransactions] = useState([]);
     const [amount, setAmount] = useState("");
     const [description, setDescription] = useState("");
-    const [chartData, setChartData] = useState(null);
+    // const [chartData, setChartData] = useState(null);
     const [totalAmount, setTotalAmount] = useState(0);
-    const [showTransactions, setShowTransactions] = useState(false);
+    const navigate = useNavigate();
+    const [selectedPeriod, setSelectedPeriod] = useState('12h');
+    const date = new Date();
+    const formattedDate = date.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
     useEffect(() => {
         getTransactions();
-        updateChartData(transactions);
+        // updateChartData(transactions);
         calculateTotalAmount(transactions);
     }, []);
 
     useEffect(() => {
         if (transactions.length > 0) {
-            updateChartData(transactions);
+            // updateChartData(transactions);
             calculateTotalAmount(transactions);
         }
     }, [transactions]);
@@ -34,7 +37,7 @@ function Home() {
     const getTransactions = () => {
         api.get("/transactions/").then((response) => response.data).then((data) => {
             setTransactions(data);
-            updateChartData(data);
+            // updateChartData(data);
             calculateTotalAmount(data);
         }).catch((error) => { alert(error); });
     }
@@ -58,72 +61,72 @@ function Home() {
         setDescription("");
     }
 
-    const updateChartData = (transactions) => {
-        const dates = transactions.map((transaction) => {
-            const transactionDate = new Date(transaction.date);
-            const year = transactionDate.getFullYear();
-            const month = String(transactionDate.getMonth() + 1).padStart(2, '0');
-            const day = String(transactionDate.getDate()).padStart(2, '0');
-            const hours = String(transactionDate.getHours()).padStart(2, '0');
-            const minutes = String(transactionDate.getMinutes()).padStart(2, '0');
-            return `${year}-${month}-${day} ${hours}:${minutes}`;
-        });
-        const totalAmounts = calculateTotalAmountAtTheTime(transactions);
+    // const updateChartData = (transactions) => {
+    //     const dates = transactions.map((transaction) => {
+    //         const transactionDate = new Date(transaction.date);
+    //         const year = transactionDate.getFullYear();
+    //         const month = String(transactionDate.getMonth() + 1).padStart(2, '0');
+    //         const day = String(transactionDate.getDate()).padStart(2, '0');
+    //         const hours = String(transactionDate.getHours()).padStart(2, '0');
+    //         const minutes = String(transactionDate.getMinutes()).padStart(2, '0');
+    //         return `${year}-${month}-${day} ${hours}:${minutes}`;
+    //     });
+    //     const totalAmounts = calculateTotalAmountAtTheTime(transactions);
 
-        setChartData({
-          labels: dates,datasets: [
-              {
-                data: totalAmounts,
-                backgroundColor: "rgba(192, 75, 192, 0.2)",
-                borderColor: "rgba(192, 75, 192, 1)",
-                borderWidth: 1,
-            },
-          ],
-        });
+    //     setChartData({
+    //       labels: dates,datasets: [
+    //           {
+    //             data: totalAmounts,
+    //             backgroundColor: "rgba(192, 75, 192, 0.2)",
+    //             borderColor: "rgba(192, 75, 192, 1)",
+    //             borderWidth: 1,
+    //         },
+    //       ],
+    //     });
 
-        if (chartRef.current) {
-          chartRef.current.destroy();
-        }
+    //     if (chartRef.current) {
+    //       chartRef.current.destroy();
+    //     }
 
-        const ctx = document.getElementById("myChart").getContext("2d");
-        chartRef.current = new Chart(ctx, {
-          type: "line",
-          data: chartData,
-          options: {
-              scales: {
-                  y: {
-                      beginAtZero: true,
-                  },
-              },
-              plugins: {
-                  legend: {
-                      display: false,
-                  },
-                  tooltip: {
-                    callbacks: {
-                        label: function (context) {
-                            const label = context.dataset.label || '';
-                            if (context.parsed.y !== null) {
-                                const transaction = transactions[context.dataIndex]; // Get the transaction at the current index
-                                const totalAmount = totalAmounts[context.dataIndex]; // Get the total amount at the current index
-                                const labelText = [
-                                    `Total Amount: ${totalAmount}`,
-                                    `Transaction Amount: ${transaction.amount}`,
-                                    `Description: ${transaction.description}`
-                                ];
-                                return labelText.join('\n');
-                            } else {
-                                return null;
-                            }
-                        }
-                    }
-                  }
-              }
-          },
-      });
-    };
+    //     const ctx = document.getElementById("myChart").getContext("2d");
+    //     chartRef.current = new Chart(ctx, {
+    //       type: "line",
+    //       data: chartData,
+    //       options: {
+    //           scales: {
+    //               y: {
+    //                   beginAtZero: true,
+    //               },
+    //           },
+    //           plugins: {
+    //               legend: {
+    //                   display: false,
+    //               },
+    //               tooltip: {
+    //                 callbacks: {
+    //                     label: function (context) {
+    //                         const label = context.dataset.label || '';
+    //                         if (context.parsed.y !== null) {
+    //                             const transaction = transactions[context.dataIndex]; // Get the transaction at the current index
+    //                             const totalAmount = totalAmounts[context.dataIndex]; // Get the total amount at the current index
+    //                             const labelText = [
+    //                                 `Total Amount: ${totalAmount}`,
+    //                                 `Transaction Amount: ${transaction.amount}`,
+    //                                 `Description: ${transaction.description}`
+    //                             ];
+    //                             return labelText.join('\n');
+    //                         } else {
+    //                             return null;
+    //                         }
+    //                     }
+    //                 }
+    //               }
+    //           }
+    //       },
+    //   });
+    // };
 
-    const chartRef = useRef(null);
+    // const chartRef = useRef(null);
     const calculateTotalAmount = (transactions) => {
         const total = transactions.reduce((acc, curr) => acc + parseInt(curr.amount), 0);
         setTotalAmount(total);
@@ -135,62 +138,67 @@ function Home() {
           total += parseInt(num.amount);
           totals.push(total);
         });
-        console.log(totals);
         return totals;
     };
 
+    const handleLogout = () => {
+        navigate("/logout");
+    };
+
     return (
-        <div>
-            {/* <div>
-                <h2>Transactions</h2>
-                {transactions.map((transaction) => (
-                    <Transaction transaction={transaction} onDelete={deleteTransaction} key={transaction.id}/>
-                    ))}
-            </div> */}
-            <h2>Total Amount in Transactions: {totalAmount}</h2>
-            <button onClick={() => setShowTransactions(!showTransactions)}>Expand History</button>
-            {showTransactions && (
-                <TableContainer>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Amount</TableCell>
-                            <TableCell>Description</TableCell>
-                            <TableCell>Licit/Illicit</TableCell>
-                            <TableCell>Action</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {transactions.map((transaction) => (
-                            <TableRow key={transaction.id}>
-                                <TableCell>{transaction.date}</TableCell>
-                                <TableCell>{transaction.amount}</TableCell>
-                                <TableCell>{transaction.description}</TableCell>
-                                <TableCell>Licit</TableCell>
-                                <TableCell>
-                                    <button onClick={() => deleteTransaction(transaction.id)}>Delete</button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            )}
-            <h2>Create a Transaction</h2>
-            <form onSubmit={createTransaction}>
-                <label htmlFor="amount">Amount:</label>
-                <br/>
-                <input type="number" id="amount" name="amount" value={amount} onChange={(e) => setAmount(e.target.value)} required/>
-                <label htmlFor="description">Description:</label>
-                <br/>
-                <textarea id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)} required></textarea>
-                <br/>
-                <input type="submit" value="Submit"/>
-            </form>
-            <h2></h2>
-            <canvas id="myChart"  width= "20%" height= "10%"></canvas>
+    <div className="container">
+      <header className="header">
+            <div className="title">
+                <h1>BTC Tracker</h1>
+                <h3>{formattedDate}</h3>
+            </div>
+            <div className="user-info">
+            {/* <img src={avatar} alt="User avatar" width={'20%'}/> */}
+            {/* <span>Username</span> */}
+            <Button className="settings" onClick={handleLogout}>Logout</Button>
+            </div>
+        </header>
+      <div className="grid">
+        <div className="block block1">
+          <h5>Current Balance: </h5>
+          <h1>${totalAmount}</h1>
         </div>
+        <div className="block block2">
+            <div className="logo">
+                <img src={btcLogo} width={'50%'}/> {/* replace with the path to the BTC logo */}
+            </div>
+            <div className="summary">
+                <BTCTracker plot={false} selectedPeriod={selectedPeriod}/>
+            </div>
+        </div>
+        <div className="block block3">
+          <h2>Buy / Sell</h2>
+          
+          <form onSubmit={createTransaction}>
+            <input type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Title" required/>
+            <input type="number" id="amount" name="amount" value={amount} placeholder="Amount" onChange={(e) => setAmount(e.target.value)} required />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="block block4">
+            <BTCTracker plot={true} selectedPeriod={selectedPeriod}/>
+            <div className="button-group">
+                <button className={selectedPeriod === '12h' ? 'selected' : ''} onClick={() => setSelectedPeriod('12h')}>12h</button>
+                <button className={selectedPeriod === '1w' ? 'selected' : ''} onClick={() => setSelectedPeriod('1w')}>1w</button>
+                <button className={selectedPeriod === '3M' ? 'selected' : ''} onClick={() => setSelectedPeriod('3M')}>3M</button>
+            </div>
+        </div>
+        <div className="block block5">
+        <h2>Transactions</h2>
+                {transactions.sort((a, b) => new Date(b.date) - new Date(a.date)).slice(0, 7).map((transaction) => (
+                    <Transaction transaction={transaction} deleteTransaction={deleteTransaction} key={transaction.id}/>
+                    ))}
+        <div className="link-container">
+            <a href="/transactions">View All Transactions</a>
+        </div>
+        </div>
+      </div>
+    </div>
     )
 }
 
