758b97ca0f9e751bf0eb305d5777ec369ca4c802
WJakubowsk <wiktor.jak97@gmail.com>
2024-05-11 18:30:21 +0200

    add GAT network

---
+++ src/model/gnn.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GATv2Conv
+
+
+class FraudDetector(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        hidden_dim: int,
+        output_dim: int,
+        heads: int,
+        dropout: float,
+    ):
+        """
+        Attention-based Graph Neural Network model for fraud detection.
+        """
+        super(FraudDetector, self).__init__()
+        self.conv1 = GATv2Conv(input_dim, hidden_dim, heads=heads)
+        self.conv2 = GATv2Conv(heads * hidden_dim, hidden_dim, heads=heads)
+
+        self.post_mp = nn.Sequential(
+            nn.Linear(heads * hidden_dim, hidden_dim),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, output_dim),
+            nn.Sigmoid(),
+        )
+        self.dropout = dropout
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv1(x, edge_index)
+        x = F.dropout(F.relu(x), p=self.dropout, training=self.training)
+
+        x = self.conv2(x, edge_index)
+        x = F.dropout(F.relu(x), p=self.dropout, training=self.training)
+
+        x = self.post_mp(x)
+        return x
