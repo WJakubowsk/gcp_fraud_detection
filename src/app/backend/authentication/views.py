0b4c67eb04ceda33b4fd63635098a923a4ac61f6
galkowskim <mikolaj.galkowski7@gmail.com>
2024-06-11 16:06:00 +0200

    reformat files

--- src/app/backend/authentication/views.py
+++ src/app/backend/authentication/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from .serializers import UserSerializer
 
